58f406cf48177c01bdcc74417f62f2a532bfe90e
Shafi D. <shafiwalsher@gmail.com>
2026-01-26 00:02:06 +0530

    changes

---
+++ client/src/components/header.jsx
@@ -0,0 +1,31 @@
+import { MenuIcon } from "lucide-react";
+import LooomLogo from "../assets/looom-logo.svg";
+
+
+const Header = () => {
+    return (
+        <>
+            {/* Scrollable content */}
+            <div className="flex flex-col md:hidden">
+                <div className="flex justify-between items-center w-full py-2.5 px-2">
+                    <div></div>
+                    <img src={LooomLogo} alt="logo" className="w-8 h-8" />
+                    <MenuIcon size={20} />
+                </div>
+
+                {/* rest of page */}
+            </div>
+
+            <header className="md:hidden sticky top-0 z-50 bg-white border-b border-black/10">
+                <div className="flex justify-around px-10 font-medium text-gray-500 py-2.5">
+                    <button>For you</button>
+                    <button>Following</button>
+                </div>
+            </header>
+        </>
+
+    )
+
+}
+
+export default Header
