2b97dfa5a14fc511ee16c1bcf61e4f05833273c9
Shafi D. <shafiwalsher@gmail.com>
2026-03-20 11:54:47 +0530

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,11 @@
-import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 1,
+});
+
+// Optional: log when a connection is made
+pool.on("connect", () => {
+  console.log("PostgreSQL connected");
 });
